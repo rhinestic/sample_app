a96a568f9737d2f2ab9444e3a9af5e36908d6b88
Christine <christine@nokomai.com>
2011-03-03 15:16:21 +0800

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
+	@content = "I know I'm at home..."
   end
 
   def contact
+	@title = "Contact"
+  end
+
+  def about
+	@title = "About"
   end
 
 end

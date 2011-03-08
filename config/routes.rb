902ff470e74a72f90ae6feaae77ea75a7f509dc6
Christine <christine@nokomai.com>
2011-03-08 16:54:07 +0800

    Removed dedault Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
+
+	match '/contact', :to => 'pages#contact'	
+	match '/about', :to => 'pages#about'
+	match '/help', :to => 'pages#help'
+
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
@@ -54,6 +55,7 @@ SampleApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+	root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
